215d7436e03f824dab74ebdefba4845d4e5f2feb
Sabelodone <sabelozondo825@gmail.com>
2024-03-07 15:02:45 +0000

    Implement a class named EVCar that extends the Car class:

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -3,20 +3,11 @@ import Car from './10-car';
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this._range = range; // Use underscore attribute version
+    this._range = range;
   }
 
-  get range() {
-    return this._range;
-  }
-
-  set range(value) {
-    this._range = value;
-  }
-
+  // eslint-disable-next-line class-methods-use-this
   cloneCar() {
-    const CarClass = super.constructor; // Get the constructor of the superclass (Car)
-    const clonedCar = new CarClass(this.brand, this.motor, this.color); // Instantiate a new Car object with the same attributes
-    return clonedCar;
+    return new Car();
   }
 }
